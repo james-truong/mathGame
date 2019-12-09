ecac193504695344e38c1bee3ac655105c05cf54
James Truong <james.truong@hotmail.com>
2019-12-09 17:45:23 -0500

    added game

---
+++ player.rb
@@ -0,0 +1,8 @@
+class Player
+  attr_accessor :lives, :name
+
+  def initialize(name)
+    @lives = 3
+    @name = name
+  end
+end
---
+++ questions.rb
@@ -0,0 +1,3 @@
+@questions = [{:question => "What is 1+1?", :answer => "2"},
+ {:question => "What is 3*3?", :answer => "9"},
+{:question => "What is 10-5?", :answer => "5"}]
